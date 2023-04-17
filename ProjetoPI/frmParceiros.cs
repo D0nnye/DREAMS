3501b332b3e0ef83558d5d5af2d563680bdf4d88
Leocadio <dosleocadio@gmail.com>
2023-04-17 09:58:51 -0300

    Att

---
+++ ProjetoPI/frmParceiros.cs
@@ -0,0 +1,351 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoPI
+{
+    public partial class frmParceiros : Form
+    {
+
+        public frmParceiros()
+        {
+            InitializeComponent();
+            desabilitaCampos();
+            carregarCombBox();
+        }
+        public void carregarCombBox()
+        {
+            cbbEstado.Items.Add("");
+            cbbEstado.Items.Add("SP");
+            cbbEstado.Items.Add("RJ");
+            cbbEstado.Items.Add("BH");
+            cbbEstado.Items.Add("BA");
+            cbbEstado.Items.Add("RN");
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            frmMenuPrincipal voltar = new frmMenuPrincipal();
+            voltar.Show();
+            this.Hide();
+        }
+
+        private void mskCEP_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                buscaCEP(mskCEP.Text);
+                txtNum.Focus();
+            }
+        }
+        public void buscaCEP(string numCEP)
+        {
+            WSCorreios.AtendeClienteClient ws = new WSCorreios.AtendeClienteClient();
+
+            try
+            {
+                WSCorreios.enderecoERP end = ws.consultaCEP(numCEP);
+
+                txtEndereco.Text = end.end;
+                txtBairro.Text = end.bairro;
+                txtCidade.Text = end.cidade;
+                cbbEstado.Text = end.uf;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Insira CEP válido!!!",
+                    "Mensagem do Sistema",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error,
+                    MessageBoxDefaultButton.Button1);
+                mskCEP.Clear();
+                mskCEP.Focus();
+
+            }
+        }
+
+        private void txtEmail_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyCode == Keys.Enter) || (e.KeyCode == Keys.Tab))
+            {
+                bool valida = validaEmail(txtEmail.Text);
+
+                if (valida == true)
+                {
+                    mskCEP.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("Insira e-mail válido",
+                    "Mensagem do Sistema",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error,
+                    MessageBoxDefaultButton.Button1);
+                    txtEmail.Clear();
+                    txtEmail.Focus();
+                }
+            }
+        }
+        public static bool validaEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            try
+            {
+
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                    RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                string DomainMapper(Match match)
+                {
+                    var idn = new IdnMapping();
+
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+            try
+            {   
+                return Regex.IsMatch(email,
+                  @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                  RegexOptions.IgnoreCase,
+                  TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            habilitarCampos();
+            btnNovo.Enabled = false;
+            btnCadastrar.Enabled = true;
+            btnVoltar.Enabled = true;
+        }
+        private void btnCadastrar_KeyDown(object sender, KeyEventArgs e)
+        {
+            verificarCampo();
+            cadastrarParceiro();
+        }
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            alterarParceiro(Convert.ToInt32(txtCodigo.Text));
+        }
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            excluirParceiro(Convert.ToInt32(txtCodigo.Text));
+        }
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            frmPesquisaFunc abrir = new frmPesquisaFunc();
+            abrir.Show();
+            this.Hide();
+        }
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+        }
+        private void btnVoltar_Click_1(object sender, EventArgs e)
+        {
+            frmMenuPrincipal abrir = new frmMenuPrincipal();
+            abrir.Show();
+            this.Hide();
+        }
+
+        public void excluirParceiro(int codPar)
+        {
+            MySqlCommand comm = new MySqlCommand();
+            comm.CommandText = "delete from tbParceiro where codPar = " + codPar + ";";
+            comm.CommandType = CommandType.Text;
+            comm.Connection = Conexao.obterConexao();
+            comm.Parameters.Clear();
+            comm.Parameters.Add("@codProd", MySqlDbType.Int32).Value = txtCodigo.Text;
+
+            DialogResult vresp = MessageBox.Show("Deseja Realizar a Exclusão?", "Mensagem do Sistema", MessageBoxButtons.YesNo,
+               MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+
+            if (vresp == DialogResult.Yes)
+            {
+                int res = comm.ExecuteNonQuery();
+                MessageBox.Show("Registro excluído com sucesso." + res);
+            }
+            else
+            {
+                MessageBox.Show("Não foi excluido.");
+            }
+            Conexao.fecharConexao();
+        }
+
+        public void alterarParceiro(int codFunc)
+        {
+            MySqlCommand comm = new MySqlCommand();
+            comm.CommandText = "update tbParceiro set cargo = @cargo , nome = @nome, email = @email, telefone = @telefone,endereco = @endereco, cnpj = @cnpj, siglaEst = @siglaEst, cep = @cep, cidade = @cidade, bairro = @bairro, numero = @numero, complemento = @complemento where codFunc = " + codFunc + ";";
+            comm.CommandType = CommandType.Text;
+            comm.Connection = Conexao.obterConexao();
+
+            comm.Parameters.Clear();
+
+            comm.Parameters.Add("@cargo", MySqlDbType.VarChar, 100).Value = txtNome.Text;
+            comm.Parameters.Add("@nome", MySqlDbType.VarChar, 100).Value = txtNome.Text;
+            comm.Parameters.Add("@email", MySqlDbType.VarChar, 100).Value = txtEmail.Text;
+            comm.Parameters.Add("@endereco", MySqlDbType.VarChar, 100).Value = txtEndereco.Text;
+            comm.Parameters.Add("@telefone", MySqlDbType.VarChar, 20).Value = mskTelefone.Text;
+            comm.Parameters.Add("@cnpj", MySqlDbType.VarChar, 14).Value = mskCNPJ.Text;
+            comm.Parameters.Add("@siglaEst", MySqlDbType.VarChar, 2).Value = cbbEstado.Text;
+            comm.Parameters.Add("@cep", MySqlDbType.VarChar, 10).Value = mskCEP.Text;
+            comm.Parameters.Add("@cidade", MySqlDbType.VarChar, 50).Value = txtCidade.Text;
+            comm.Parameters.Add("@bairro", MySqlDbType.VarChar, 50).Value = txtBairro.Text;
+            comm.Parameters.Add("@numero", MySqlDbType.VarChar, 14).Value = txtNum.Text;
+            comm.Parameters.Add("@complemento", MySqlDbType.VarChar, 50).Value = txtComplemento.Text;
+
+
+            int res = comm.ExecuteNonQuery();
+            MessageBox.Show("Registro alterado com sucesso." + res);
+            Conexao.fecharConexao();
+        }
+
+        public void verificarCampo()
+        {
+            if (txtNome.Text == "")
+            {
+                MessageBox.Show("Favor inserir valores");
+            }
+            else if (txtEmail.Text == "")
+            {
+                MessageBox.Show("Favor inserir valores");
+            }
+
+            if (txtNome.Text.Equals("") || txtEmail.Text.Equals("")
+              || txtEndereco.Text.Equals("") || mskTelefone.Text.Equals("(  )      -")
+                || mskCNPJ.Text.Equals("  .   .   /    -  ") || mskCEP.Text.Equals("     -")
+                || txtCidade.Text.Equals("") || txtBairro.Text.Equals("") ||
+                txtNum.Text.Equals("") || cbbEstado.Text.Equals(""))
+            {
+                MessageBox.Show("Favor inserir valores!!!",
+                    "Mensagem do Sistema",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information,
+                    MessageBoxDefaultButton.Button1);
+                txtNome.Focus();
+            }
+
+        }
+        public void cadastrarParceiro()
+        {
+            MySqlCommand comm = new MySqlCommand();
+
+            comm.CommandText = "insert into tbParceiro(cargo, nome, email, endereco, telefone, cnpj, cep, siglaEst, cidade, bairro, numero, complemento)" +
+    "values(@cargo, @nome, @email, @endereco, @telefone, @cnpj, @cep, @siglaEst, @cidade, @bairro, @numero, @complemento); ";
+
+            comm.CommandType = CommandType.Text;
+
+            comm.Parameters.Clear();
+
+            comm.Parameters.Add("@nome", MySqlDbType.VarChar, 100).Value = txtNome.Text;
+            comm.Parameters.Add("@email", MySqlDbType.VarChar, 100).Value = txtEmail.Text;
+            comm.Parameters.Add("@endereco", MySqlDbType.VarChar, 100).Value = txtEndereco.Text;
+            comm.Parameters.Add("@telefone", MySqlDbType.VarChar, 20).Value = mskTelefone.Text;
+            comm.Parameters.Add("@cnpj", MySqlDbType.VarChar, 14).Value = mskCNPJ.Text;
+            comm.Parameters.Add("@siglaEst", MySqlDbType.VarChar, 2).Value = cbbEstado.Text;
+            comm.Parameters.Add("@cep", MySqlDbType.VarChar, 10).Value = mskCEP.Text;
+            comm.Parameters.Add("@cidade", MySqlDbType.VarChar, 50).Value = txtCidade.Text;
+            comm.Parameters.Add("@bairro", MySqlDbType.VarChar, 50).Value = txtBairro.Text;
+            comm.Parameters.Add("@numero", MySqlDbType.VarChar, 14).Value = txtNum.Text;
+            comm.Parameters.Add("@complemento", MySqlDbType.VarChar, 50).Value = txtComplemento.Text;
+
+            comm.CommandType = CommandType.Text;
+
+            comm.Connection = Conexao.obterConexao();
+
+            int i = comm.ExecuteNonQuery();
+
+            MessageBox.Show("Parceiro cadastrado com sucesso!!!" + i);
+            limparCampos();
+            desabilitaCampos();
+
+            Conexao.fecharConexao();
+        }
+
+        public void desabilitaCampos()
+        {
+            txtCodigo.Enabled = false;
+            txtNome.Enabled = false;
+            txtEmail.Enabled = false;
+            txtEndereco.Enabled = false;
+            mskTelefone.Enabled = false;
+            mskCNPJ.Enabled = false;
+            mskCEP.Enabled = false;
+            cbbEstado.Enabled = false;
+            txtCidade.Enabled = false;
+            txtBairro.Enabled = false;
+            txtNum.Enabled = false;
+            txtComplemento.Enabled = false;
+            btnNovo.Enabled = true;
+            btnCadastrar.Enabled = false;
+            btnAlterar.Enabled = false;
+            btnExcluir.Enabled = false;
+            btnPesquisar.Enabled = true;
+            btnLimpar.Enabled = false;
+            btnVoltar.Enabled = true;
+        }
+        public void habilitarCampos()
+        {
+            txtNome.Enabled = true;
+            txtEmail.Enabled = true;
+            txtEndereco.Enabled = true;
+            mskTelefone.Enabled = true;
+            mskCNPJ.Enabled = true;
+            mskCEP.Enabled = true;
+            cbbEstado.Enabled = true;
+            txtCidade.Enabled = true;
+            txtBairro.Enabled = true;
+            txtNum.Enabled = true;
+            txtComplemento.Enabled = true;
+            btnNovo.Enabled = true;
+            btnCadastrar.Enabled = false;
+            btnAlterar.Enabled = false;
+            btnExcluir.Enabled = false;
+            btnPesquisar.Enabled = true;
+            btnLimpar.Enabled = true;
+            btnVoltar.Enabled = true;
+            txtNome.Focus();
+        }
+        public void limparCampos()
+        {
+            txtNome.Text = "";
+            txtEmail.Text = "";
+            txtEndereco.Text = "";
+            mskTelefone.Text = "";
+            mskCNPJ.Text = "";
+            mskCEP.Text = "";
+            cbbEstado.Text = "";
+            txtCidade.Text = "";
+            txtBairro.Text = "";
+            txtNum.Clear();
+            txtComplemento.Clear();
+        }
+
+    }
+}
---
+++ ProjetoPI/frmPesquisaFunc.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ProjetoPI
+{
+    public partial class frmPesquisaFunc : Form
+    {
+        public frmPesquisaFunc()
+        {
+            InitializeComponent();
+            txtDescricao.Enabled = false;
+            rdbCodigo.Checked = false;
+            rdbNome.Checked = false;
+        }
+        private void rdbCodigo_CheckedChanged(object sender, EventArgs e)
+        {
+            txtDescricao.Enabled = true;
+            txtDescricao.Focus();
+        }
+        private void rdbNome_CheckedChanged(object sender, EventArgs e)
+        {
+            txtDescricao.Enabled = true;
+            txtDescricao.Focus();
+        }
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            if (rdbCodigo.Checked)
+            {
+                ltbItensPesquisados.Items.Clear();
+                //ltbItensPesquisados.Items.Add(txtDescricao.Text);
+                pesquisaCodigo(txtDescricao.Text);
+
+            }
+            else if (rdbNome.Checked)
+            {
+                //ltbItensPesquisados.Items.Clear();
+                //ltbItensPesquisados.Items.Add(txtDescricao.Text);
+                pesquisaNome(txtDescricao.Text);
+            }
+            else  
+            {
+               
+            }
+          
+        }
+        public bool acessaBanco(string nomeUsu)
+        {
+            MySqlCommand comm = new MySqlCommand();
+            comm.CommandText = "select * from tbUsuario where nomeUsu = '" + nomeUsu + "';";
+            comm.CommandType = CommandType.Text;
+            comm.Connection = Conexao.obterConexao();
+
+            MySqlDataReader DR;
+
+            DR = comm.ExecuteReader();
+
+            bool resposta = DR.HasRows;
+
+            Conexao.fecharConexao();
+
+            return resposta;
+        }
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtDescricao.Enabled = false;
+            rdbNome.Checked = false;
+            rdbCodigo.Checked = false;
+            txtDescricao.Clear();
+            ltbItensPesquisados.Items.Clear();
+        }
+        public void pesquisaCodigo(string codigo)
+        {
+            bool validaCod = acessaBanco(codigo);
+            if (validaCod)
+            {
+                MySqlCommand comm = new MySqlCommand();
+                comm.CommandText = "select * from tbFuncionarios where codFunc = " + codigo + ";";
+                comm.CommandType = CommandType.Text;
+                comm.Connection = Conexao.obterConexao();
+
+                MySqlDataReader DR;
+                DR = comm.ExecuteReader();
+                DR.Read();
+
+                ltbItensPesquisados.Items.Clear();
+
+                ltbItensPesquisados.Items.Add(DR.GetString(1));
+            }
+            else
+            {
+                acessaBanco(txtDescricao.Text);
+                MessageBox.Show("O usuario não existe no Banco!!!",
+                    "Aviso do sistema",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error,
+                    MessageBoxDefaultButton.Button1);
+            }
+            Conexao.fecharConexao();
+        }
+
+        
+        public void pesquisaNome(string nome)
+        {
+            bool validaNome = acessaBanco(nome);
+            if (validaNome)
+            {
+                MySqlCommand comm = new MySqlCommand();
+                comm.CommandText = "select * from tbFuncionarios where nome like '%" + nome + "%';";
+                comm.CommandType = CommandType.Text;
+                comm.Connection = Conexao.obterConexao();
+
+                MySqlDataReader DR;
+
+                DR = comm.ExecuteReader();
+
+                ltbItensPesquisados.Items.Clear();
+
+                while (DR.Read())
+                {
+                    ltbItensPesquisados.Items.Add(DR.GetString(1));
+                }
+            }
+            else
+            {
+                acessaBanco(txtDescricao.Text);
+                MessageBox.Show("O usuario não existe no Banco!!!",
+                    "Aviso do sistema",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error,
+                    MessageBoxDefaultButton.Button1);
+            }
+            Conexao.fecharConexao();
+        }
+
+        private void ltbItensPesquisados_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string valor = ltbItensPesquisados.SelectedItem.ToString();
+            //MessageBox.Show("Resultado: " + valor);
+            frmfuncionarios abrir = new frmfuncionarios (valor);
+            abrir.Show();
+            this.Hide();
+        }
+
+        private void btnPesquisaUsu_Click(object sender, EventArgs e)
+        {
+                //Abrir outra janela
+                frmCarregaDataGridDBUsu abrir = new frmCarregaDataGridDBUsu();
+                abrir.Show();
+                this.Hide();
+           
+        }
+
+        private void btnPesquisaFunc_Click(object sender, EventArgs e)
+        {
+
+            frmCarregaDataGridDB abrir = new frmCarregaDataGridDB();
+            abrir.Show();
+            this.Hide();
+        }
+
+        
+
+
+    }
+}
